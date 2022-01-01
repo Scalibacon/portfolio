21907f0f7ddc6ec0255299aaca6a5e05115839df
Matheus F Nascimento <matheusferreira_mfn@hotmail.com>
2022-01-01 16:16:58 -0300

    headerzin simples

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,25 @@
+import styles from '../../styles/Header.module.scss';
+import logo from '../../assets/nerdola.svg';
+import { FaCog } from 'react-icons/fa';
+
 const Header = () => {
   return (
-    <h1>Eu sou o Header!</h1>
+    <header className={styles.header}>      
+      <figure>
+        <img src={logo} />
+      </figure>
+
+      <nav>
+        <ul>
+          <li><a href='#'>Início</a></li>
+          <li><a href='#'>Resumão</a></li>
+          <li><a href='#'>Habilidades</a></li>
+          <li><a href='#'>Projetos</a></li>
+        </ul>
+      </nav>
+
+      <span className={styles.settings}><FaCog size="2rem"/></span>
+    </header>
   )
 }
 
