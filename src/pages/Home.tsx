ca9f9eac55c0d023460ccf809b9f11a2884a4ff4
Matheus F Nascimento <matheusferreira_mfn@hotmail.com>
2022-01-01 16:17:25 -0300

    início do home com as roles e início das quotes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,38 @@
 import Header from "../components/Header";
+import styles from '../styles/Home.module.scss';
+import Nerdola from '../assets/nerdola.svg';
 
 const Home = () => {
   return (
     <>
       <Header />
-      <h2>Eu sou a homepage!</h2>
+      <main className={styles.wrapper}>
+        <header>
+          <figure className={styles.intro}>
+            <img src={Nerdola}/>
+            <figcaption>
+              <p>MATHEUS</p>
+              <mark>"SCALI"</mark>
+              <p>NASCIMENTO</p>
+            </figcaption>
+          </figure>
+
+          <span className={styles.roles}>
+            <span>Desenvolvedor Web </span> 
+            <span>Desenvolvedor Full Stack</span>
+            <span>Eterno Aprendiz</span>
+          </span>
+        </header>
+
+        <section className={styles.testimonial}>
+          <input type="range" min="1" max="5" />
+          <ul className={styles.coments}>
+            <blockquote>Menózin é bala nesses bagulho de tecnologia aí, sehloko família</blockquote>
+            <hr/>
+            <cite>Marcão da ZL</cite>
+          </ul>
+        </section>        
+      </main>
     </>
   )    
 }
