d28b069f11104e1dc853a31f3e126fe41c82fb3f
Matheus F Nascimento <matheusferreira_mfn@hotmail.com>
2022-01-01 22:32:41 -0300

    seção de comentários

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,43 @@
 import Header from "../components/Header";
 import styles from '../styles/Home.module.scss';
 import Nerdola from '../assets/nerdola.svg';
+import { FaQuoteLeft, FaQuoteRight } from 'react-icons/fa';
+import { useState } from "react";
+
+type comment = {
+  author: string,
+  quote: string
+}
 
 const Home = () => {
+  const [activeComment, setActiveComment] = useState(1);
+
+  const comments = [
+    {
+      author: 'Leo Stronda',
+      quote: 'C@r#lh#, o maluco é brabo!'
+    },    
+    {
+      author: 'Minha Mãe',
+      quote: 'Não fez mais do que sua obrigação, moleque!'
+    },
+    {
+      author: 'Evandson Cruz',
+      quote: 'Agora eu sei por que sua cabeça é grande... Só assim pra caber tanta coisa nela.'
+    },
+    {
+      author: 'Meu amigo imaginário',
+      quote: 'O Matheus é um cara super divertido, inteligente e manja demais de programação!'
+    },
+  ] as comment[];
+
   return (
     <>
       <Header />
       <main className={styles.wrapper}>
         <header>
           <figure className={styles.intro}>
-            <img src={Nerdola}/>
+            <img src={Nerdola} alt="Nerdola"/>
             <figcaption>
               <p>MATHEUS</p>
               <mark>"SCALI"</mark>
@@ -25,11 +53,21 @@ const Home = () => {
         </header>
 
         <section className={styles.testimonial}>
-          <input type="range" min="1" max="5" />
-          <ul className={styles.coments}>
-            <blockquote>Menózin é bala nesses bagulho de tecnologia aí, sehloko família</blockquote>
-            <hr/>
-            <cite>Marcão da ZL</cite>
+          <input 
+            type="range" min="0" max="3" 
+            value={activeComment}
+            onChange={e => setActiveComment(parseInt(e.target.value))}
+          />
+          <ul className={styles.comments}>
+            <li>
+              <blockquote>
+                <FaQuoteLeft className={styles.openQuote} />
+                { comments[activeComment].quote }
+                <FaQuoteRight  className={styles.closeQuote}/>
+              </blockquote>
+              <hr/>
+              <cite>{ comments[activeComment].author }</cite>
+            </li>
           </ul>
         </section>        
       </main>
