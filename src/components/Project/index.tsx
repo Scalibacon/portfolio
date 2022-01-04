060bfb06542b185aee6143a3b39579f2f7431ed5
Matheus F Nascimento <matheusferreira_mfn@hotmail.com>
2022-01-04 15:31:12 -0300

    efeito de fadein no main dos project

--- src/components/Project/index.tsx
+++ src/components/Project/index.tsx
@@ -1,9 +1,18 @@
 import styles from '../../styles/Project.module.scss';
+import { FiGithub } from 'react-icons/fi';
 
 const Project = () => {
   return (
-    <article className={styles.project}>
-      eae
+    <article className={styles.project}>      
+      <main>
+        <h3>Projeto do Tetheus</h3>
+
+        <p>Um projeto teste só pra eu ver como fica nessa bagaça. Neste projeto foram utilizadas as seguintes tecnologias: Node.js no lado do servidor, React juntamente com HTML e CSS no lado do cliente, com o auxílio do fabuloso TypeScript.</p>
+
+        <a>
+          <FiGithub size={"1.3rem"}/>GitHub
+        </a>
+      </main>     
     </article>
   )
 }
