4cfba6abd29dd8c781f95ce8b9438188ff990fb5
Matheus F Nascimento <matheusferreira_mfn@hotmail.com>
2022-04-07 23:46:40 -0300

    correções no uso do service de email

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -16,7 +16,8 @@ const Contact = () => {
   const messageInput = useRef<HTMLTextAreaElement>(null);
 
   const sendMail = async () => {
-    return new Promise( async (resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
+      //'http://localhost:3333/email'
       const result = await fetch('https://scali-email-service.herokuapp.com/email', {
         method: 'POST',
         headers: {
@@ -30,22 +31,21 @@ const Contact = () => {
           phone: phone
         })
       });
-
-      console.log('result ', result);
-
-      if(result.status === 200 || result.status === 202){
+      
+      // const formattedResult = await result.json();
+      if (result.status === 200 || result.status === 202) {
         return resolve(true);
-      } 
+      }
 
-      return reject(false);           
+      return reject(false);
     })
   }
 
-  const handleSendMail = async (event: SyntheticEvent) => {  
+  const handleSendMail = async (event: SyntheticEvent) => {
     event.preventDefault();
 
-    if(!validateForm()) return;
-    
+    if (!validateForm()) return;
+
     const promise = sendMail();
 
     toast.promise(
@@ -53,26 +53,26 @@ const Contact = () => {
       {
         pending: 'Tô encaminhando seu e-mail pro chefe...',
         success: 'E-mail enviado com sucesso!',
-        error: 'Vish! Ocorreu algum erro. Verifique se o e-mail está certo e tente novamente.'
+        error: 'Vish! Ocorreu algum erro. Verifique se o e-mail está certo e tente novamente.'       
       }
     );
   }
 
   const validateForm = () => {
-    
-    if(!emailInput.current?.checkValidity()){
+
+    if (!emailInput.current?.checkValidity()) {
       toast.error("E-mail inválido!");
       emailInput.current?.classList.add(styles.error);
       return false;
     }
 
-    if(!nameInput.current?.checkValidity()){
+    if (!nameInput.current?.checkValidity()) {
       toast.error("Digita seu nome aí, pô");
       nameInput.current?.classList.add(styles.error);
       return false;
     }
 
-    if(!messageInput.current?.checkValidity()){
+    if (!messageInput.current?.checkValidity()) {
       toast.error("Seria bom escrever uma mensagem, né?");
       messageInput.current?.classList.add(styles.error);
       return false;
@@ -82,63 +82,63 @@ const Contact = () => {
   }
 
   const handleSetPhone = (value: string) => {
-    let formatedPhone:string;
+    let formattedPhone: string;
 
-    formatedPhone = value.replace(/[^-0-9*()+ ]/g,'');
-    setPhone(formatedPhone)
+    formattedPhone = value.replace(/[^-0-9*()+ ]/g, '');
+    setPhone(formattedPhone);
   }
 
   return (
     <main className={styles.contactForm}>
       <section className={styles.socials}>
-        
+
         <a href="https://www.linkedin.com/in/matheus-f-nascimento/" target="_blank" rel="noreferrer">
-          <FaLinkedin size="2rem"/>
+          <FaLinkedin size="2rem" />
           <figcaption>/matheus-f-nascimento</figcaption>
         </a>
 
         <a href="https://web.facebook.com/matheus.ferreiradonascimento/" target="_blank" rel="noreferrer">
-          <FaFacebookSquare size="2rem"/>
+          <FaFacebookSquare size="2rem" />
           <figcaption>Matheus Ferreira</figcaption>
         </a>
 
         <a href="https://github.com/Scalibacon" target="_blank" rel="noreferrer">
-          <FaGithub size="2rem"/>
+          <FaGithub size="2rem" />
           <figcaption>/scalibacon</figcaption>
         </a>
 
         <a href="https://www.youtube.com/channel/UCC5yrRtwpkoCRBoAHkvJsgg" target="_blank" rel="noreferrer">
-          <FaYoutube size="2rem"/>
+          <FaYoutube size="2rem" />
           <figcaption>Matheus Ferreira</figcaption>
-        </a>              
+        </a>
 
         <a href="https://steamcommunity.com/id/scalibacon/" target="_blank" rel="noreferrer">
-          <FaSteam size="2rem"/>
+          <FaSteam size="2rem" />
           <figcaption>/scalibacon</figcaption>
         </a>
-        
-        
+
+
         <a href="https://api.whatsapp.com/send?phone=5511981116773&amp;text=Salve!%20Bora%20ter%20uma%20troca%C3%A7%C3%A3o%20franca%20de%20ideias?" target="_blank" rel="noreferrer">
-          <FaWhatsapp size="2rem"/>
+          <FaWhatsapp size="2rem" />
           <figcaption>+55 (11)98111-6773</figcaption>
-        </a>        
-        
+        </a>
+
       </section>
 
-      <form onSubmit={ event => handleSendMail(event) }>
-        <input type="text" placeholder="Digite seu nome" required className={styles.name} 
-          ref={nameInput} value={ name } onChange={ e => setName(e.target.value) } onFocus={ e => e.target.classList.remove(styles.error) }
+      <form onSubmit={event => handleSendMail(event)}>
+        <input type="text" placeholder="Digite seu nome" required className={styles.name}
+          ref={nameInput} value={name} onChange={e => setName(e.target.value)} onFocus={e => e.target.classList.remove(styles.error)}
         />
-        <input type="email" placeholder="Digite seu e-mail" required className={styles.email} 
-          ref={emailInput} value={ email } onChange={ e => setEmail(e.target.value) } onFocus={ e => e.target.classList.remove(styles.error) }
+        <input type="email" placeholder="Digite seu e-mail" required className={styles.email}
+          ref={emailInput} value={email} onChange={e => setEmail(e.target.value)} onFocus={e => e.target.classList.remove(styles.error)}
         />
-        <input type="text" placeholder="Digite seu telefone (opcional)" required className={styles.phone} 
-          ref={phoneInput} value={ phone } onChange={ e => handleSetPhone(e.target.value) } onFocus={ e => e.target.classList.remove(styles.error) }
-        />                
-        <textarea placeholder="Digite sua mensagem para mim >.<" required className={styles.text} 
-          ref={messageInput} value={ message } onChange={ e => setMessage(e.target.value) } onFocus={ e => e.target.classList.remove(styles.error) }
+        <input type="text" placeholder="Digite seu telefone (opcional)" className={styles.phone}
+          ref={phoneInput} value={phone} onChange={e => handleSetPhone(e.target.value)} onFocus={e => e.target.classList.remove(styles.error)}
+        />
+        <textarea placeholder="Digite sua mensagem para mim >.<" required className={styles.text}
+          ref={messageInput} value={message} onChange={e => setMessage(e.target.value)} onFocus={e => e.target.classList.remove(styles.error)}
         ></textarea>
-        <input type="submit" value="Enviar e-mail"/>
+        <input type="submit" value="Enviar e-mail" />
       </form>
 
       <ToastContainer
